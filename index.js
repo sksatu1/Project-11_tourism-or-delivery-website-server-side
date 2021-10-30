11f565c2e568de0d06ef198c9327472c342afb69
sksatu1 <satueee18@gmail.com>
2021-10-30 14:43:52 +0600

    first

---
+++ index.js
@@ -0,0 +1,79 @@
+const express = require('express');
+const { MongoClient } = require('mongodb');
+require('dotenv').config();
+const cors = require('cors');
+const ObjectId = require('mongodb').ObjectId
+
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+// middleware-----
+app.use(cors());
+app.use(express.json());
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.kzjok.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+async function run() {
+
+    try {
+        await client.connect();
+        console.log('data base connected successfully');
+
+        const database = client.db("tourism");
+        const placesCollection = database.collection("places");
+
+
+        // get API ----------------------
+        app.get('/places', async (req, res) => {
+            const cursor = placesCollection.find({});
+            const places = await cursor.toArray();
+            res.send(places);
+        })
+
+        app.get('/places/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const place = await placesCollection.findOne(query);
+            console.log('load place with id: ', id);
+            res.send(place);
+        })   
+
+        // post API -------------------------
+        app.post('/places', async (req, res) => {
+            const newplace = req.body;
+            const result = await placesCollection.insertOne(newplace);
+
+            console.log('got new user', req.body);
+            console.log('added user', result);
+            res.json(result);
+        })
+
+        // delete API ------------------------
+        app.delete('/places/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await placesCollection.deleteOne(query);
+            console.log('deleting user with id : ', result);
+            res.json(result);
+        });
+    }
+
+    finally {
+        // await client.close();
+    }
+
+
+}
+
+run().catch(console.dir);
+
+app.get('/', (req, res) => {
+    res.send('hello from the Assignment 11 server');
+})
+
+app.listen(port, () => {
+    console.log('listening to port ', port);
+})
