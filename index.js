1e67a4ba2baf727774d9e9135ebe5d00322f2dec
sksatu1 <satueee18@gmail.com>
2021-10-30 23:57:42 +0600

    5th

--- index.js
+++ index.js
@@ -34,6 +34,12 @@ async function run() {
             res.send(places);
         })
 
+        app.get('/orders', async (req, res) => {
+            const cursor = orderCollection.find({});
+            const orders = await cursor.toArray();
+            res.send(orders);
+        })
+
         app.get('/places/:id', async (req, res) => {
             const id = req.params.id;
             const query = { _id: ObjectId(id) };
